df35a1f27d14fda80e223388c9f90b397538f9fb
MartinCapocasale <martincapocasale@gmail.com>
2017-03-19 23:49:20 -0300

    add java class

---
+++ factores-primos/Factores-Primos.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FactoresPrimos {
+
+	public static void main(String[] args){
+				
+		System.out.println("Ingrese un numero:");
+		Scanner ingreso = new Scanner(System.in);
+		
+		int factorPrimo;
+		int numero;
+		System.out.println(("Factores Primos de " + (numero = ingreso.nextInt())) + ":");
+		
+		for(factorPrimo = 2; factorPrimo<=numero; factorPrimo++){
+			
+			while(numero % factorPrimo == 0){
+				System.out.println(factorPrimo);
+				numero /= factorPrimo;
+				
+			}
+			
+		}		
+	}
+
+}
