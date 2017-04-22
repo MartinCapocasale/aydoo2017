bddd73888e62dba262dd3baec3219020c2be24fd
MartinCapocasale <martincapocasale@gmail.com>
2017-04-22 19:32:09 -0300

    Se agrega metodo montoACobrar

--- ejercicio_libreria/src/main/java/ar/edu/untref/aydoo/Libreria.java
+++ ejercicio_libreria/src/main/java/ar/edu/untref/aydoo/Libreria.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Libreria {
@@ -7,6 +8,36 @@ public class Libreria {
 	List<Cliente> listaDeClientes = new LinkedList<Cliente>();
 	List<Suscripcion> listaDeSuscripciones = new LinkedList<Suscripcion>();
 	
+	
+	public double CalcularMontoACobrar(String mes, Cliente unCliente) {
+		double montoDescuento = 0;
+		double montoACobrar = 0;
+		Iterator<Compra> it = listaDeCompras.iterator();
+		
+		while(it.hasNext()){
+			Compra compra = it.next();
+			
+			if (compra.getMes() == mes && compra.getMiCliente() == unCliente){
+				montoACobrar += compra.getNuevoProducto().getPrecio() * compra.getCantidad();
+			}
+		}
+		
+		Iterator<Suscripcion> otroIt = listaDeSuscripciones.iterator();
+		
+		while(otroIt.hasNext()){
+			Suscripcion suscripcion = otroIt.next();
+		
+			if (suscripcion.getMes() == mes && suscripcion.getMiCliente() == unCliente){
+				if (suscripcion.getTiempoDeSuscripcion() == "anual"){
+					montoDescuento = (suscripcion.getNuevoProducto().getPrecio() * suscripcion.getCantidadDeEjemplares())*0.2;
+				}
+				
+				montoACobrar += suscripcion.getNuevoProducto().getPrecio() * suscripcion.getCantidadDeEjemplares();
+			}
+		}
+		return montoACobrar - montoDescuento;
+	}
+
 	public void nuevaCompra(Compra nuevaCompra) {
 		listaDeCompras.add(nuevaCompra);
 	}
