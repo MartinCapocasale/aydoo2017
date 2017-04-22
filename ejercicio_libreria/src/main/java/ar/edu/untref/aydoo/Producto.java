18523999c5cfb3840b2585c9ee6530b76857af42
MartinCapocasale <martincapocasale@gmail.com>
2017-04-22 17:35:24 -0300

    Se clase padre Producto

---
+++ ejercicio_libreria/src/main/java/ar/edu/untref/aydoo/Producto.java
@@ -0,0 +1,25 @@
+
+public class Producto {
+	
+	private String periodicidad;
+	private double precioDeVenta;
+
+	public Producto(double precio, String nuevaPeriodicidad){
+		this.periodicidad = nuevaPeriodicidad;
+		this.setPrecio(precio);
+		
+	}
+	
+	public void setPrecio(double precio) {
+		this.precioDeVenta = precio;
+	}
+	
+	public double getPrecio() {
+		return precioDeVenta;
+	}
+	
+	public String getPeriodicidad(){
+		return this.periodicidad;
+	}
+
+}
