c2c240254b7135b74383af5d6fa1cd4ab0aeebe5
MartinCapocasale <martincapocasale@gmail.com>
2017-05-01 23:43:40 -0300

    modificacion en FactoresPrimosTest

--- factores-primos-3/src/test/java/ar/edu/untref/aydoo/FactoresPrimosTest.java
+++ factores-primos-3/src/test/java/ar/edu/untref/aydoo/FactoresPrimosTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class FactoresPrimosTest {
 
 	@Test
-	public void testFactoresPrimos90ConFormaDeOrdenAscendente(){
+	public void testFactoresPrimos90(){
 		int numero = 90;
 		LinkedList<Integer> factoreo90 = new LinkedList<Integer>();
 		factoreo90.add(2);
@@ -17,14 +17,12 @@ public class FactoresPrimosTest {
 
 		FactoresPrimos factoresPrimos = new FactoresPrimos();
     	factoresPrimos.calcularFactor(numero);
-    	
-    	LinkedList<Integer> resultado = factoresPrimos.getFormaDeOrden("asc");
-    	Assert.assertEquals(factoreo90,resultado);
+ 	
+    	Assert.assertEquals(factoreo90,factoresPrimos.imprimir());
 		
 	}
-	
 	@Test
-	public void testFactoresPrimos360ConFormaDeOrdenAscendente(){
+	public void testFactoresPrimos360(){
 		int numero = 360;
 		LinkedList<Integer> factoreo360 = new LinkedList<Integer>();
 		factoreo360.add(2);
@@ -37,44 +35,37 @@ public class FactoresPrimosTest {
 		FactoresPrimos factoresPrimos = new FactoresPrimos();
     	factoresPrimos.calcularFactor(numero);
  	
-    	LinkedList<Integer> resultado = factoresPrimos.getFormaDeOrden("asc");
-    	Assert.assertEquals(factoreo360,resultado);
+    	Assert.assertEquals(factoreo360,factoresPrimos.imprimir());
 		
 	}
-	
 	@Test
-	public void testFactoresPrimos12ConFormaDeOrdenDescendete(){
+	public void testFactoresPrimos12(){
 		int numero = 12;
 		LinkedList<Integer> factoreo12 = new LinkedList<Integer>();
+		factoreo12.add(2);
+		factoreo12.add(2);
 		factoreo12.add(3);
-		factoreo12.add(2);
-		factoreo12.add(2);
 
 
 		FactoresPrimos factoresPrimos = new FactoresPrimos();
     	factoresPrimos.calcularFactor(numero);
  	
-    	LinkedList<Integer> resultado = factoresPrimos.getFormaDeOrden("des");
-    	Assert.assertEquals(factoreo12,resultado);
+    	Assert.assertEquals(factoreo12,factoresPrimos.imprimir());
 		
 	}
-	
 	@Test
-	public void testFactoresPrimos90ConFormaDeOrdenDescendete(){
-		int numero = 90;
-		LinkedList<Integer> factoreo90 = new LinkedList<Integer>();
-		factoreo90.add(5);
-		factoreo90.add(3);
-		factoreo90.add(3);
-		factoreo90.add(2);
+	public void testFactoresPrimos2(){
+		int numero = 2;
+		LinkedList<Integer> factoreo2 = new LinkedList<Integer>();
+		factoreo2.add(2);
 		
 		FactoresPrimos factoresPrimos = new FactoresPrimos();
     	factoresPrimos.calcularFactor(numero);
-    	
-    	LinkedList<Integer> resultado = factoresPrimos.getFormaDeOrden("des");
-    	Assert.assertEquals(factoreo90,resultado);
+ 	
+    	Assert.assertEquals(factoreo2,factoresPrimos.imprimir());
 		
 	}
+
 }
 
 	
