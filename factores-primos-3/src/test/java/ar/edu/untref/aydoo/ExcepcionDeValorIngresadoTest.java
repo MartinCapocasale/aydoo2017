0193d8bbad4fac7fb6dc436d24e87d61695f2520
MartinCapocasale <martincapocasale@gmail.com>
2017-05-02 00:56:27 -0300

    modificacion de tests

--- factores-primos-3/src/test/java/ar/edu/untref/aydoo/ExcepcionDeValorIngresadoTest.java
+++ factores-primos-3/src/test/java/ar/edu/untref/aydoo/ExcepcionDeValorIngresadoTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 public class ExcepcionDeValorIngresadoTest {
 	
 	@Test(expected = ExcepcionDeValorIngresado.class)
-	public void intentoCalcularConUnValorMenorACero(){
+	public void intentoCalcularConUnValorInvalido(){
 		FactoresPrimos factoresPrimos = new FactoresPrimos();
 		factoresPrimos.calcularFactor(-2);
 	}
