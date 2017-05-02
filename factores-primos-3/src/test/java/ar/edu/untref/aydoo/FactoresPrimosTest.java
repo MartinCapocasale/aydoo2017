ca8accf0bbf25e2a516fab7222fd01858f904e34
MartinCapocasale <martincapocasale@gmail.com>
2017-05-02 00:55:14 -0300

    modificacion de tests

--- factores-primos-3/src/test/java/ar/edu/untref/aydoo/FactoresPrimosTest.java
+++ factores-primos-3/src/test/java/ar/edu/untref/aydoo/FactoresPrimosTest.java
@@ -1,5 +1,3 @@
-import java.util.LinkedList;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,60 +7,44 @@ public class FactoresPrimosTest {
 	@Test
 	public void testFactoresPrimos90(){
 		int numero = 90;
-		LinkedList<Integer> factoreo90 = new LinkedList<Integer>();
-		factoreo90.add(2);
-		factoreo90.add(3);
-		factoreo90.add(3);
-		factoreo90.add(5);
-
+		
 		FactoresPrimos factoresPrimos = new FactoresPrimos();
-    	factoresPrimos.calcularFactor(numero);
+    	String resultado = factoresPrimos.calcularFactor(numero);
  	
-    	Assert.assertEquals(factoreo90,factoresPrimos.imprimir());
+    	Assert.assertEquals("2 3 3 5 ",resultado);
 		
 	}
+
 	@Test
 	public void testFactoresPrimos360(){
 		int numero = 360;
-		LinkedList<Integer> factoreo360 = new LinkedList<Integer>();
-		factoreo360.add(2);
-		factoreo360.add(2);
-		factoreo360.add(2);
-		factoreo360.add(3);
-		factoreo360.add(3);
-		factoreo360.add(5);
-
+	
 		FactoresPrimos factoresPrimos = new FactoresPrimos();
-    	factoresPrimos.calcularFactor(numero);
+		String resultado = factoresPrimos.calcularFactor(numero);
  	
-    	Assert.assertEquals(factoreo360,factoresPrimos.imprimir());
+    	Assert.assertEquals("2 2 2 3 3 5 ",resultado);
 		
 	}
+	
 	@Test
 	public void testFactoresPrimos12(){
 		int numero = 12;
-		LinkedList<Integer> factoreo12 = new LinkedList<Integer>();
-		factoreo12.add(2);
-		factoreo12.add(2);
-		factoreo12.add(3);
-
 
 		FactoresPrimos factoresPrimos = new FactoresPrimos();
-    	factoresPrimos.calcularFactor(numero);
+		String resultado = factoresPrimos.calcularFactor(numero);
  	
-    	Assert.assertEquals(factoreo12,factoresPrimos.imprimir());
+    	Assert.assertEquals("2 2 3 ",resultado);
 		
 	}
+	
 	@Test
 	public void testFactoresPrimos2(){
 		int numero = 2;
-		LinkedList<Integer> factoreo2 = new LinkedList<Integer>();
-		factoreo2.add(2);
 		
 		FactoresPrimos factoresPrimos = new FactoresPrimos();
-    	factoresPrimos.calcularFactor(numero);
+		String resultado = factoresPrimos.calcularFactor(numero);
  	
-    	Assert.assertEquals(factoreo2,factoresPrimos.imprimir());
+    	Assert.assertEquals("2 ",resultado);
 		
 	}
 
