1d06fa9b3c544efce5dc388151e7b346cf70355f
MartinCapocasale <martincapocasale@gmail.com>
2017-05-02 02:15:41 -0300

    se agrega clase para pasar Argumentos

---
+++ factores-primos-3/src/main/java/ar/edu/untref/aydoo/PasajeDeArgumentos.java
@@ -0,0 +1,50 @@
+
+public class PasajeDeArgumentos {
+
+		private int numero = 0;
+		private String formato = "pretty";
+		private String nombreDeArchivo = "";
+		private String orden = "";
+		private String argumentos[];
+
+		public PasajeDeArgumentos(String args[]) {
+			this.numero = Integer.parseInt(args[0]);
+			this.argumentos = args;
+		}
+
+		public void analizarArgumentos() {
+
+			for (String argumentoActual : argumentos) {
+				
+				if (argumentoActual.startsWith("--format=")) {
+					this.formato = argumentoActual.substring(9).toLowerCase();
+				} 
+				
+				else if (argumentoActual.startsWith("--sort=")) {
+					this.orden = argumentoActual.substring(7);
+				} 
+				
+				else if (argumentoActual.startsWith("--output-file=")) {
+					this.nombreDeArchivo = argumentoActual.substring(14);
+				}
+
+			}
+		}
+
+		public String getFormato() {
+			return formato;
+		}
+
+		public String getNombreDeArchivo() {
+			return nombreDeArchivo;
+		}
+
+		public String getOrden() {
+			return orden;
+		}
+
+		public int getNumero() {
+			return numero;
+		}
+
+	}
